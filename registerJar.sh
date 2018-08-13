583f19f3b8b3f3951eae7f581b2b645af8e448e4
Lee Burch <ltburch2000@gmail.com>
2018-08-13 14:39:01 -0500

    Added BH service jars (to register locally)

---
+++ registerJar.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=BHRegisterJar/user-web-service-client-1.0.0.jar -DgroupId=com.bhc.apps.user -DartifactId=user-web-service-client -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=BHRegisterJar/entirex-web-service-client-1.0.3-SNAPSHOT.jar -DgroupId=com.bhc.entirex -DartifactId=entirex-web-service-client -Dversion=1.0.3-SNAPSHOT -Dpackaging=jar
+
